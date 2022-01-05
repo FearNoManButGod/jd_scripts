6a1e23cca0897459b64d6d387e3ac4d7daafbdc9
FearNoManButGod <491708819@qq.com>
2022-01-05 17:32:19 +0800

    移除活动

--- activity/jd_medal.js
+++ activity/jd_medal.js
@@ -4,9 +4,9 @@ cron 16 7,16 * * * jd_medal.js
 TG频道：https://t.me/sheeplost
  */
 const $ = new Env('农场集勋章');
-const notify = $.isNode() ? require('./sendNotify') : '';
+const notify = $.isNode() ? require('../sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message = ''; ;
 if ($.isNode()) {
