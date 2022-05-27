07cadeaad06b68dd73e9df48b266d3e8623a667a
FearNoManButGod <491708819@qq.com>
2022-05-27 15:24:11 +0800

    哎

--- jd_joy_park_run.ts
+++ jd_joy_park_run.ts
@@ -9,7 +9,7 @@ new Env('极速版汪汪赛跑');
 **/
 
 import {get, post, o2s, requireConfig, wait} from './TS_USER_AGENTS'
-import {H5ST} from "./h5st"
+import {H5ST} from "./utils/h5st"
 import {existsSync, readFileSync} from "fs";
 import {getDate} from "date-fns";
 
